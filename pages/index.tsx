9a2cc8c3e42c9feabd119202347521800a5ecd5b
shimotsu4431 <shimotsu13@gmail.com>
2022-10-05 13:25:26 +0900

    Revert "add useAvatar"
    This reverts commit 579f98dfc3a9629ade1c56d163716cac7def7a39.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import type { NextPage } from 'next';
-import { useEffect } from 'react';
+import { ChangeEvent, useCallback, useEffect, useState } from 'react';
 import clsx from 'clsx';
 import styles from '../styles/Home.module.css';
+import { colorPalette } from '../config/colorPalette';
 import { FaRandom } from 'react-icons/fa';
 import { useMicrocms } from '../hooks/useMicrocms';
-import { useAvatar } from '../hooks/useAvatar';
 
 export type Variants =
   | 'marble'
@@ -26,22 +26,36 @@ export type HEX = `#${string}`;
 
 const Index: NextPage = () => {
   const [data, setData, submitData] = useMicrocms();
-  const [
-    mounted,
-    name,
-    selectedVariant,
-    colors,
-    handleSelectVariant,
-    handleRandomPalette,
-    handleInputName,
-    makeColorCodes,
-  ] = useAvatar(data);
+  const [name, setName] = useState<string>('');
+  const [selectedVariant, setSelectedVariant] = useState<Variants>('beam');
+  const [mounted, setMounted] = useState<boolean>(false);
+
+  const [colors, setColors] = useState<HEX[]>(colorPalette[0]);
+
+  useEffect(() => {
+    if (data) {
+      setName(data.name);
+      setSelectedVariant(data.variant);
+      setColors(data.colors);
+
+      // data.name だけ取得が遅れるので暫定的にこうしておく
+      setTimeout(() => {
+        setMounted(true);
+      }, 300);
+    }
+  }, [data]);
+
+  const makeColorCodes = () => {
+    const arr = colors.map((c) => c.slice(1));
+
+    return arr.join(',');
+  };
 
   const handleSubmit = () => {
     const iconSize = 100;
     const imageUrl = `https://source.boringavatars.com/${selectedVariant}/${iconSize}/${encodeURI(
       name
-    )}?colors=${makeColorCodes(colors)}`;
+    )}?colors=${makeColorCodes()}`;
 
     submitData({
       imageUrl,
@@ -51,10 +65,29 @@ const Index: NextPage = () => {
     });
   };
 
+  const handleSelectVariant = (item: Variants) => {
+    setSelectedVariant(item);
+  };
+
+  const handleRandomPalette = useCallback(() => {
+    const MIN = 0;
+    const MAX = 11;
+    const randomNum = Math.floor(Math.random() * (MAX + 1 - MIN)) + MIN;
+
+    setColors(colorPalette[randomNum]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleInputName = (e: ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
   useEffect(() => {
     handleSubmit();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [name, selectedVariant, colors[0]]);
+
+  if (data === null) return null;
 
   return (
     <div className={styles.container}>
@@ -64,9 +97,7 @@ const Index: NextPage = () => {
             <img
               src={
                 (data && data.imageUrl) ||
-                `https://source.boringavatars.com/${selectedVariant}/100/${name}?colors=${makeColorCodes(
-                  colors
-                )}`
+                `https://source.boringavatars.com/${selectedVariant}/100/${name}?colors=${makeColorCodes()}`
               }
               alt={(data && data.name) || ''}
             />
